af90faa5cc8a44b8165fec17bac6c0d2487597a8
Mo Tanveer <mo@motanveer.com>
2019-01-27 01:45:39 -0500

    Complete essential add UI and park object model

--- scripts/parks-plus.js
+++ scripts/parks-plus.js
@@ -9,18 +9,25 @@
 ██║     ██║  ██║██║  ██║██║  ██╗███████║    ██║     ███████╗╚██████╔╝███████║
 ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝    ╚═╝     ╚══════╝ ╚═════╝ ╚══════╝
                                                                              
+ParksPlus is a simple web project made by @MohTanveer.
 
+This project uses the following resources and technologies:
 
+- Vanilla JS + some minimal jQuery
+- Bootstrap 4
+- Flat UI Bootstrap Theme (Free Version)
+- Animate.CSS
+- Font Awesome (Free Version) 
  */
 
 
 
 /*-------------------------------[UI] Data Structure     ----------------------------*/
 
-class park
-{
-    constructor(codeName, parkLocation, actvitySelection, friendsList)
-    {
+class park {
+
+
+    constructor(codeName, parkLocation, actvitySelection, friendsList) {
         this.codeName = codeName;
         this.parkLocation = parkLocation;
         this.actvitySelection = actvitySelection;
@@ -28,7 +35,33 @@ class park
     }
 }
 
-var friendsList = [];
+var parkList = []; //Stores the entire list of park objects
+var friendsList = []; //Stores the list of everyone's who attening the park visit
+var actvitySelection = [];
+
+var codeName = document.querySelector("#trip-codename"); //Gets Codename Value from Input
+var parkLocation = document.querySelector("#park-location");
+
+
+
+/*------------------------------- Build Park Object  ---------------------------*/
+
+
+var submit = document.querySelector("#submit");
+submit.addEventListener("click", function () {
+    
+    //console.log(parkLocation.value);
+    //console.log(codeName)
+    //getCheckbox();
+    //console.log(actvitySelection);
+    parkList.push(new park(codeName.value,parkLocation.value,actvitySelection, friendsList));
+
+        
+
+    console.log(parkList)
+});
+
+
 
 
 
@@ -77,17 +110,20 @@ function updateCheckbox(index) {
 
         if (checkboxes[counter].checked == true) {
             activityCards[index].style.border = "#2ecc71 3.5px solid";
+            actvitySelection.push(activityCards[index]);
         }
 
     }
 
     else {
         checkboxes[counter].checked = false;
+        
 
     }
 
 
 }
+
 
 
 /*-------------------------------[UI] Add Friends  ---------------------------
@@ -96,30 +132,51 @@ checks and unchecks checkboxes and shows a selection state via updated styles
 
  */
 
- var friendAdd = document.querySelector("#add-friend");
- friendAdd.addEventListener("click", function()
- {
-  addFriend();
- });
+var friendAdd = document.querySelector("#add-friend");
+friendAdd.addEventListener("click", function () {
+    addFriend();
+});
 
- 
- function addFriend()
- {
 
+function addFriend() {
+
+    //-- The following will add the friend name to an array 
     let friend = document.querySelector("#friend-input");
     friendsList.push(friend.value);
 
+
+    //-- The following will append the added name to the table, serving as a visual confirmation 
+    // Note there is no connection between the array, used for storage and the table
     const list = document.querySelector('#friend-table');
     const row = document.createElement('tr');
-    
+
     row.innerHTML =
         `<td>${friend.value}</td>
          <td><span class="fui-cross"></span></td>`;
-    
-         list.appendChild(row);
+
+    list.appendChild(row);
 
     friend.value = '';
+
+
     console.log(friendsList);
 
+}
 
+/*-------------------------------[UI] Clear Fields  ---------------------------
+ 
+checks and unchecks checkboxes and shows a selection state via updated styles
+
+ */
+
+ function clearFields()
+ {
+    for (var index = 0; index < checkboxes.length; index++)
+    {
+        checkboxes[index].checked = false;
+    }
+
+    codeName.value = '';
+    parkLocation.value = '';
+    
  }
